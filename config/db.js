f2bf701c483e61a053588ac8e816182dd23bac0b
G-andres18 <116139499+G-andres18@users.noreply.github.com>
2022-12-21 12:33:54 -0500

    first commit

---
+++ config/db.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env'});
+
+
+
+const conectarDB = async () => {
+
+    try{
+
+        await mongoose.connect(process.env.DB_Mongo, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            
+        })
+        
+        console.log('BD Conectada');
+
+    } catch (error){
+
+        console.log(error);
+        process.exit(1); // detenemos la app
+    }
+
+    
+}
+
+module.exports = conectarDB
---
+++ index.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const conectarDB = require('./config/db');
+const { use } = require('./routes/producto');
+const cors = require("cors");
+
+
+// Creamos el servidor
+
+const app = express();
+
+// Conectamos a la base de datos
+
+conectarDB();
+
+app.use(cors())
+
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'));
+
+
+app.listen(4000, () => {
+
+    console.log('El servidor esta corriendo perfectamente')
+})
